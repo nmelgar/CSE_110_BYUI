ef25d03dd9bf7e1f08c5d947a007ce5436639ef4
nmelgar <nmelgar16013@gmail.com>
2022-01-05 16:12:10 -0600

    16:11--01 prove assignment, favorite color

---
+++ week1/favorite_color.py
@@ -0,0 +1,7 @@
+name = input("Hello, What's your name?: ")
+color = input("Hey " + name.title() + "!, What's your favorite color?: ")
+text = "\nAwesome!, your favorite color is " + color.title()
+goodbye = "\nI love " + color + " color too, well...See you later!"
+
+print(text)
+print(goodbye)
