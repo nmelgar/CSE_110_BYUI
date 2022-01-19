17bcfbc43abd5160391c3b4f892b2e884acf76cd
nmelgar <nmelgar16013@gmail.com>
2022-01-18 22:12:34 -0600

    22:12--working with numbers

---
+++ week3/numbers.py
@@ -0,0 +1,15 @@
+# pi = 3.14159
+# print(pi)
+
+first_num = input("Please enter a number: ")
+second_num = input("Please enter another number: ")
+#combine the above statement in one line
+#second_num = int(input("Please enter another number: "))
+print(int(first_num) + int(second_num))
+#you can use float also
+print(float(first_num) + float(second_num))
+# print(first_num ** second_num)
+
+#convert number to string
+days_in_February = 28
+print(str(days_in_February) + " total days in February")
