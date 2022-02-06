a24ddf70d6236850764890940e943d934361c6d3
nmelgar <nmelgar16013@gmail.com>
2022-02-05 21:19:04 -0600

    21:18--advanced text-based game

--- week5/text_based_game_1.py
+++ week5/text_based_game_1.py
@@ -1,10 +1,19 @@
 print("*********************************")
 print("¡¡WORLD IS UNDER A NEW PANDEMIC!!")
+print("People are becoming zombies that eat brains!")
 print("You will have to do everything to survive")
 print("*********************************")
 
 print("You wake up in a hospital without knowing what happened, ",
 "\nmany ideas come to your mind and you don't know what you will do.")
-hide_run = input("What will you do, HIDE or RUN?")
+hide_run = input("What will you do, HIDE or RUN?: ")
 if hide_run.lower() == "run":
     print(f"you chose {hide_run.capitalize()}")
+    print(f"You decided to {hide_run}, now you need to choose your weapon")
+
+if hide_run.lower() == "hide":
+    print(f"you chose {hide_run.capitalize()}")
+    print(f"You decided to {hide_run}, now you need to choose your weapon")
+
+else:
+    print("You died :(")
