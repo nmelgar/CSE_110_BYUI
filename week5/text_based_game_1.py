34a3fbfba8d2555582c177a2fe6a572ca62a46ad
nmelgar <nmelgar16013@gmail.com>
2022-02-11 17:58:31 -0600

    17:58--working with choosing partners

--- week5/text_based_game_1.py
+++ week5/text_based_game_1.py
@@ -6,6 +6,11 @@ vehicle = ["green", "blue"]
 weapon = ["bat", "gun"]
 partner = ["martha", "ken"]
 
+
+def death_function():
+    print("You died :(")
+
+
 print("*********************************")
 print("¡¡WORLD IS UNDER A NEW PANDEMIC!!")
 print("People are becoming zombies that eat brains!")
@@ -13,15 +18,27 @@ print("You will have to do everything to survive")
 print("*********************************")
 
 print("You wake up in a hospital without knowing what happened, ",
-"\nmany ideas come to your mind and you don't know what you will do.")
+      "\nmany ideas come to your mind and you don't know what you will do.")
 hide_run = input("What will you do, HIDE or RUN?: ")
-if hide_run.lower() == "run":
+# 1st IF-ELSE BLOCK
+if hide_run.lower() == "hide":
+    print(f"You decided to {hide_run}. As you remain at the hospital, "
+          "you find out \nthere are many people there, they are as confused as you."
+          "\nYou realize few of them have name-tags in their chests, so you think "
+          "\nit would be nice to have a partner to go with you.")
+    partner_decision = input("What partner will you choose? MARTHA or KEN?: ")
+    if partner_decision.lower == "martha":
+        print(f"You choose {partner_decision} as partner. While you speak about what "
+              "you will do. You listen a lot of weird noises, people begin to run "
+              "towards the exit")
+    elif partner_decision.lower == "ken":
+        print(f"You choose {partner_decision} as partner. While you speak about what "
+              "you will do. You listen a lot of weird noises, people begin to run "
+              "towards the exit")
+    else:
+        death_function()
+elif hide_run.lower() == "run":
     print(f"you chose {hide_run.capitalize()}")
-    print(f"You decided to {hide_run}, now you need to choose your weapon")
-
-elif hide_run.lower() == "hide":
-    print(f"you chose {hide_run.capitalize()}")
-    print(f"You decided to {hide_run}, now you need to choose your weapon")
-
+    print(f"You decided to {hide_run}, ")
 else:
-    print("You died :(")
+    death_function()
