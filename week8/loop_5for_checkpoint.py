bc001a0940a22c8049d1a81f73d29a739bf44967
nmelgar <nmelgar16013@gmail.com>
2022-02-23 23:19:57 -0600

    23:19--not done with the multiplication table :(

---
+++ week8/loop_5for_checkpoint.py
@@ -0,0 +1,14 @@
+print("Exercise 1---------------")
+colors = ["red", "blue", "green", "yellow"]
+for color in colors:
+    print(color)
+print("")
+
+print("Exercise 2---------------")
+for number in range(1, 9):
+    print(number)
+print("")
+
+print("Exercise 3---------------")
+for number2 in range(2, 21, 2):
+    print(number2)
--- week8/multiplication_tables.py
+++ week8/multiplication_tables.py
@@ -1,7 +1,2 @@
-columns_and_rows = int(input("How many and rows do you want in your multiplication table? "))
-
-for columns in range(1, columns_and_rows + 1):
-    print(f"{columns}", end="  ")
-    
-    
-    
+rows = int(
+    input("How many and rows do you want in your multiplication table? "))
