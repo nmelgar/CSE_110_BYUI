65d15a47c0d5bc1e7d87334cd280172b77037c1c
nmelgar <nmelgar16013@gmail.com>
2022-03-05 16:20:41 -0600

    16:20--working smallest and largest numbers oflist

--- week9/lists_4smallest_number.py
+++ week9/lists_4smallest_number.py
@@ -1,7 +1,7 @@
-largest = 9999999
+smallest = 9999999
 my_list = [1, 3, 7, 8, 9]
 for value in my_list:
-    if value < largest:
-        largest = value
+    if value < smallest:
+        smallest = value
 
-print(f"The smallest number is {largest}")
+print(f"The smallest number is {smallest}")
