00acb7657b35f9b9a94678969e6046184eb45197
nmelgar <nmelgar16013@gmail.com>
2022-03-08 08:05:30 -0600

    08:05--bank accounts working, lets do it again

--- week10/bank_accounts.py
+++ week10/bank_accounts.py
@@ -9,9 +9,17 @@ while run:
     add_account = str(input("What is the name of this account? "))
     if add_account != "quit":
         bank_accounts.append(add_account)
-        add_balance = int(input("What is the balance? "))
+        add_balance = float(input("What is the balance? "))
         balances.append(add_balance)
     elif add_account == "quit":
         run = False
 
 print("Account information")
+for index in range(len(bank_accounts)):
+    add_account = bank_accounts[index]
+    add_balance = balances[index]
+    print(add_account, " - ", add_balance)
+    total = add_balance + add_balance
+    average = total / len(balances)
+print(f"Total: ${total}")
+print(f"Average: ${average}")
