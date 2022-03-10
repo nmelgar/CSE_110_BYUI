34f694634384b1a481ea2e2dd4c77c99944a45e4
nmelgar <nmelgar16013@gmail.com>
2022-03-09 20:17:07 -0600

    20:16--shopping cart is done!

--- week10/shopping_cart.py
+++ week10/shopping_cart.py
@@ -20,66 +20,86 @@ while run:
     add_price = ""
 
     if action <= len(menu):
-        #IF 0 as INVALID NUMBER
+        # IF 0 as INVALID NUMBER
         if action == 0:
             print("Please try a valid number!")
 
         # 1. ADD ITEM
         if action == 1:
+            print("***********************************")
             add_item = str(input("\nWhat item would you like to add? "))
             items.append(add_item)
             add_price = (float(input(f"What is the price of {add_item}? ")))
             prices.append(add_price)
             print(f"'{add_item.capitalize()}' has been added to the cart")
+            print("\n***********************************")
 
         # 2. VIEW CART
         if action == 2:
             if len(items) == 0:
-                print("The shopping cart is empty")
+                print("***********************************")
+                print("\nThe shopping cart is empty")
+                print("\n***********************************")
             else:
                 total_items = 1
-                print("The contents of the shopping cart are:")
+                print("***********************************")
+                print("\nThe contents of the shopping cart are:")
                 for index in range(len(items)):
                     add_item = items[index]
                     add_price = prices[index]
-                    print(f"{index +1}. {add_item.capitalize()} - ${add_price: .2f}")
+                    print(
+                        f"{index +1}. {add_item.capitalize()} - ${add_price: .2f}")
 
-                    #count the number of items in the cart
-                    total_items += index 
-                    if total_items == 1:
-                        print(f"\nThere is {total_items} item in the cart")
-                    else:
-                        print(f"\nThere are {total_items} items in the cart")
+                    # count the number of items in the cart
+                total_items += index
+                if total_items == 1:
+                    print(f"\nThere is {total_items} item in the cart")
+                    print("\n***********************************")
+                else:
+                    print(f"\nThere are {total_items} items in the cart")
+                    print("\n***********************************")
 
-        #3. REMOVE AN ITEM
+        # 3. REMOVE AN ITEM
         if action == 3:
             if len(items) == 0:
-                print("There are not items to remove")
+                print("***********************************")
+                print("\nThere are not items to remove")
+                print("\n***********************************")
             else:
-                print("")
-                remove_item = int(input("Which item would you like to remove? "))
+                print("***********************************")
+                remove_item = int(
+                    input("\nWhich item would you like to remove? "))
                 remove_item -= 1
                 if remove_item >= len(items):
                     print("Item is not in the list")
+                    print("\n***********************************")
                 else:
                     items.pop(remove_item)
                     prices.pop(remove_item)
                     print(f"Item has been deleted")
+                    print("\n***********************************")
 
-        #4. COMPUTE TOTAL
+        # 4. COMPUTE TOTAL
         total_cost = 0
         if action == 4:
             if len(items) == 0:
-                print("There are not items in the list to get the total")
+                print("***********************************")
+                print("\nThere are not items in the list to get the total")
+                print("\n***********************************")
             else:
                 for price in prices:
                     total_cost += price
-                print(f"The total price of the items in the shopping cart is ${total_cost: .2f}")
+                print("***********************************")
+                print(
+                    f"\nThe total price of the items in the shopping cart is ${total_cost:.2f}")
+                print("\n***********************************")
 
         # 5. QUIT
         if action == 5:
             run = False
-            print("Thank you, come back again!")
+            print("***********************************")
+            print("\nThank you, come back again!")
+            print("\n***********************************")
 
     else:
         print("Please try a valid number!")
